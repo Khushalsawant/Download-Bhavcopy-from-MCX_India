9b30ff675aecbdb4e57c00d5794ba0723ae0a78b
Khushal Sawant <47523964+Khushalsawant@users.noreply.github.com>
2019-07-16 18:30:52 +0530

    Add files via upload

--- MCX_BhavCopy.py
+++ MCX_BhavCopy.py
@@ -16,12 +16,13 @@ from selenium.webdriver.common.by import By
 import time
 from datetime import date
 import pandas as pd
+import os
 
 
 start_dt = date(2019, 2, 20)
 end_dt = date.today()
 date_range_df = pd.date_range(start_dt, end_dt).strftime("%Y%m%d")
-print(date_range_df)
+#print(date_range_df)
 
 url_mcx_bhavcopy = "https://www.mcxindia.com/market-data/bhavcopy"
 
@@ -34,69 +35,70 @@ chrome_options.add_argument("--headless")  # Runs Chrome in headless mode.
 chrome_options.add_argument('--no-sandbox') # # Bypass OS security model
 chrome_options.add_argument('disable-infobars')
 chrome_options.add_argument('--disable-extensions')
-
+    
 '''
-http://allselenium.info/file-downloads-python-selenium-webdriver/
+    http://allselenium.info/file-downloads-python-selenium-webdriver/
 '''
-
+    
 #C:\Users\khushal\Downloads\MCX_Datewise
 chrome_options.add_experimental_option("prefs", {
-  "download.default_directory": r"D:\MCX_Datewise",
-  "download.prompt_for_download": False,
-  "download.directory_upgrade": True,
-  "safebrowsing.enabled": False,
-  'safebrowsing.disable_download_protection': True
-})
+    "download.default_directory": r"D:\MCX_Datewise",
+     "download.prompt_for_download": False,
+      "download.directory_upgrade": True,
+      "safebrowsing.enabled": False,
+      'safebrowsing.disable_download_protection': True
+    })
 
-driver = webdriver.Chrome(chrome_options=chrome_options)
 
-actions=ActionChains(driver)
-
-#driver.maximize_window()
-driver.get(url_mcx_bhavcopy)
-
-wait = WebDriverWait(driver, 5)
-
-# add missing support for chrome "send_command"  to selenium webdriver
-driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
-
-params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath':  r"D:\MCX_Datewise"}}
-command_result = driver.execute("send_command", params)
-
-#driver.execute_script("document.getElementById('cph_InnerContainerRight_C001_txtDate_hid_val').value = '20190710';")
-
-driver.execute_script("document.getElementById('cph_InnerContainerRight_C001_txtDate_hid_val').type = 'text';")
-#//input[@id='txtDate']
-#//*[@id='cph_InnerContainerRight_C001_txtDate_hid_val']
-element_mcx_date = driver.find_element_by_xpath("//*[@id='cph_InnerContainerRight_C001_txtDate_hid_val']")
-driver.execute_script('''
-                      var element_mcx_date = arguments[0];
-    var value = arguments[1];
-    element_mcx_date.value = value;
-                      ''', element_mcx_date, '20190702')
-#element_mcx_date = driver.find_element_by_css_selector("input#cph_InnerContainerRight_C001_txtDate_hid_val")
-#element_mcx_date.clear()
-#element_mcx_date.send_keys("20190710")
-#element_mcx_date.send_keys(Keys.RETURN)  
-
-element_show_btn= driver.find_element_by_xpath("//*[@id='btnShowDatewise']")#.click()
-actions.click(element_show_btn)
-actions.perform()
-time.sleep(10)
-
-element_csv_download = driver.find_element_by_xpath("//*[@id='cph_InnerContainerRight_C001_lnkExpToCSV']")
-actions.click(element_csv_download)
-actions.perform()
-time.sleep(15)
-
-print("driver.current_url = ",driver.current_url)
-
-'''
-if os.path.exists(filePath):
-    os.remove(filePath)
-else:
-    print("Can not delete the file as it doesn't exists")
-'''
-
-#driver.close()
-#driver.quit()
+for i in range(len(date_range_df)):
+    filename = os.path.expanduser('~') + 'D:\MCX_Datewise\BhavCopyDateWise_'+str(date_range_df[i]) + '.csv'
+    if os.path.exists(filename):
+         os.remove(filename)
+    else:
+        #print("Sorry, I can not remove %s file." % filename)
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+        
+        actions=ActionChains(driver)
+        
+        #driver.maximize_window()
+        driver.get(url_mcx_bhavcopy)
+        
+        wait = WebDriverWait(driver, 5)
+        
+        # add missing support for chrome "send_command"  to selenium webdriver
+        driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+        
+        params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath':  r"D:\MCX_Datewise"}}
+        command_result = driver.execute("send_command", params)
+        
+        #driver.execute_script("document.getElementById('cph_InnerContainerRight_C001_txtDate_hid_val').value = '20190710';")
+        
+        driver.execute_script("document.getElementById('cph_InnerContainerRight_C001_txtDate_hid_val').type = 'text';")
+        #//input[@id='txtDate']
+        #//*[@id='cph_InnerContainerRight_C001_txtDate_hid_val']
+        element_mcx_date = driver.find_element_by_xpath("//*[@id='cph_InnerContainerRight_C001_txtDate_hid_val']")
+        driver.execute_script('''
+                              var element_mcx_date = arguments[0];
+            var value = arguments[1];
+            element_mcx_date.value = value;
+                              ''', element_mcx_date, date_range_df[i])
+        #element_mcx_date = driver.find_element_by_css_selector("input#cph_InnerContainerRight_C001_txtDate_hid_val")
+        #element_mcx_date.clear()
+        #element_mcx_date.send_keys("20190710")
+        #element_mcx_date.send_keys(Keys.RETURN)  
+        
+        element_show_btn= driver.find_element_by_xpath("//*[@id='btnShowDatewise']")#.click()
+        actions.click(element_show_btn)
+        actions.perform()
+        time.sleep(10)
+        
+        element_csv_download = driver.find_element_by_xpath("//*[@id='cph_InnerContainerRight_C001_lnkExpToCSV']")
+        actions.click(element_csv_download)
+        actions.perform()
+        time.sleep(15)
+        
+        print("driver.current_url = ",driver.current_url)
+        
+        driver.close()
+        driver.quit()
+        time.sleep(5)
